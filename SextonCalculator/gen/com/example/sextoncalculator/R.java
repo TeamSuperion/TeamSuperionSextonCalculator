ba1203a2d34a049891c9666e1d4290fa2ddf3ef6
Justin Springer <jespringer@csbsju.edu>
2012-10-17 20:27:19 -0500

    continued work on the browse page

--- SextonCalculator/gen/com/example/sextoncalculator/R.java
+++ SextonCalculator/gen/com/example/sextoncalculator/R.java
@@ -15,35 +15,41 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int amountRemaining_textView=0x7f07000e;
-        public static final int back_button=0x7f07000c;
-        public static final int browse_button=0x7f070018;
-        public static final int calorie_button=0x7f070019;
-        public static final int cashPay_button=0x7f070012;
-        public static final int cashPay_editText=0x7f070013;
-        public static final int checkout_button=0x7f070007;
-        public static final int drinks_button=0x7f070003;
-        public static final int editText1=0x7f07000a;
-        public static final int editText2=0x7f070014;
-        public static final int entree_button=0x7f070001;
-        public static final int flexPay_button=0x7f070011;
+        public static final int amountRemaining_textView=0x7f070014;
+        public static final int back_button=0x7f070012;
+        public static final int browse_button=0x7f07001e;
+        public static final int calorie_button=0x7f07001f;
+        public static final int cashPay_button=0x7f070018;
+        public static final int cashPay_editText=0x7f070019;
+        public static final int checkout_button=0x7f07000d;
+        public static final int drinks_button=0x7f070009;
+        public static final int drinks_content=0x7f070008;
+        public static final int drinks_drawer=0x7f070007;
+        public static final int editText1=0x7f070010;
+        public static final int editText2=0x7f07001a;
+        public static final int entree_button=0x7f070003;
+        public static final int entree_content=0x7f070002;
+        public static final int entree_drawer=0x7f070001;
+        public static final int flexPay_button=0x7f070017;
         public static final int header=0x7f070000;
-        public static final int home_button=0x7f070005;
-        public static final int home_text=0x7f070016;
-        public static final int maxCalories_textView=0x7f070009;
-        public static final int menu_settings=0x7f07001c;
-        public static final int payButtonBar=0x7f07000f;
-        public static final int punchPay_button=0x7f070010;
-        public static final int punchValue_textView=0x7f070015;
-        public static final int punch_button=0x7f070017;
-        public static final int random_button=0x7f070008;
-        public static final int reset_button=0x7f070006;
-        public static final int sides_button=0x7f070002;
-        public static final int start_button=0x7f07001b;
-        public static final int textView1=0x7f07001a;
-        public static final int totalCalories_textView=0x7f07000b;
-        public static final int totalPrice_textView=0x7f070004;
-        public static final int totalRemaining_textView=0x7f07000d;
+        public static final int home_button=0x7f07000b;
+        public static final int home_text=0x7f07001c;
+        public static final int maxCalories_textView=0x7f07000f;
+        public static final int menu_settings=0x7f070022;
+        public static final int payButtonBar=0x7f070015;
+        public static final int punchPay_button=0x7f070016;
+        public static final int punchValue_textView=0x7f07001b;
+        public static final int punch_button=0x7f07001d;
+        public static final int random_button=0x7f07000e;
+        public static final int reset_button=0x7f07000c;
+        public static final int sides_button=0x7f070006;
+        public static final int sides_content=0x7f070005;
+        public static final int sides_drawer=0x7f070004;
+        public static final int start_button=0x7f070021;
+        public static final int textView1=0x7f070020;
+        public static final int totalCalories_textView=0x7f070011;
+        public static final int totalPrice_textView=0x7f07000a;
+        public static final int totalRemaining_textView=0x7f070013;
     }
     public static final class layout {
         public static final int activity_browse=0x7f030000;
@@ -66,14 +72,14 @@ public final class R {
     public static final class string {
         public static final int amountRemaining_textView=0x7f04000e;
         public static final int app_name=0x7f040000;
-        public static final int back_button=0x7f04001d;
+        public static final int back_button=0x7f04001c;
         public static final int browse_button=0x7f040005;
         public static final int calorie_button=0x7f040004;
-        public static final int cashPay_button=0x7f040011;
+        public static final int cashPay_button=0x7f040010;
         public static final int checkout_button=0x7f040008;
-        public static final int drinks_button=0x7f04001c;
+        public static final int drinks_button=0x7f04001d;
         public static final int entree_button=0x7f04001a;
-        public static final int flexPay_button=0x7f040010;
+        public static final int flexPay_button=0x7f040011;
         public static final int home_button=0x7f040006;
         public static final int home_text=0x7f040019;
         public static final int maxCalories_textView=0x7f04000b;
@@ -88,11 +94,11 @@ public final class R {
         public static final int title_activity_browse=0x7f040017;
         public static final int title_activity_calorie=0x7f040016;
         public static final int title_activity_checkout=0x7f040018;
-        public static final int title_activity_home=0x7f040014;
+        public static final int title_activity_home=0x7f040013;
         public static final int title_activity_main=0x7f040002;
         public static final int title_activity_punch=0x7f040015;
         public static final int totalCalories_textView=0x7f04000c;
-        public static final int totalPrice_textView=0x7f040013;
+        public static final int totalPrice_textView=0x7f040014;
         public static final int totalRemaining_textView=0x7f04000d;
     }
     public static final class style {
