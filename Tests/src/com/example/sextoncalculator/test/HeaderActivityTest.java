a35f400192f641e1e3dcfd608017e36dbf062bdb
Justin Springer <jespringer@csbsju.edu>
2012-12-11 14:51:42 -0600

    Fixing the folder mess up

---
+++ Tests/src/com/example/sextoncalculator/test/HeaderActivityTest.java
@@ -0,0 +1,15 @@
+package com.example.sextoncalculator.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import com.example.sextoncalculator.*;
+
+public class HeaderActivityTest extends
+		ActivityInstrumentationTestCase2<HeaderActivity> {
+
+	public HeaderActivityTest() {
+		super("com.example.sextoncalculator", HeaderActivity.class);
+	}
+	
+	
+	
+}
---
+++ Tests/src/com/example/sextoncalculator/test/HomeActivityTest.java
@@ -0,0 +1,15 @@
+package com.example.sextoncalculator.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import com.example.sextoncalculator.*;
+
+public class HomeActivityTest extends
+		ActivityInstrumentationTestCase2<HomeActivity> {
+
+	public HomeActivityTest () {
+		super("com.example.sextoncalculator", HomeActivity.class);
+	}
+	
+	
+	
+}
