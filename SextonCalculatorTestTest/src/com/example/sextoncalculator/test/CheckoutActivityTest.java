906a5e4b1ed01db019b09a5952b6da261a9dea65
Justin Springer <jespringer@csbsju.edu>
2012-12-11 15:12:54 -0600

    Try to open the tests project

---
+++ SextonCalculatorTestTest/src/com/example/sextoncalculator/test/CheckoutActivityTest.java
@@ -0,0 +1,16 @@
+package com.example.sextoncalculator.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import com.example.sextoncalculator.*;
+
+public class CheckoutActivityTest extends
+		ActivityInstrumentationTestCase2<CheckoutActivity> {
+	
+	public CheckoutActivityTest() {
+		super("com.example.sextoncalculator", CheckoutActivity.class);
+	}
+	
+	
+	
+
+}
---
+++ SextonCalculatorTestTest/src/com/example/sextoncalculator/test/MainActivityTest.java
@@ -0,0 +1,12 @@
+package com.example.sextoncalculator.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import com.example.sextoncalculator.*;
+
+public class MainActivityTest extends
+		ActivityInstrumentationTestCase2<MainActivity> {
+
+	public MainActivityTest () {
+		super("com.example.sextoncalculator", MainActivity.class);
+	}
+}
